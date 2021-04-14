e0629caadc5bbf6c52cc9d2634238d85dea8cf21
Leif Sandstrom <leif.sandstrom@husqvarnagroup.com>
2021-04-14 10:07:00 +0200

    works

--- tests/integration/test_registration_interface.py
+++ tests/integration/test_registration_interface.py
@@ -18,22 +18,27 @@ def parse_client_registration(server_output):
         lifetime: 300 sec
         objects: /1 (1.1), /1/0, /2/0, /3/0, /4/0, /5/0, /6/0, /7/0, /31024 (1.0), /31024/10, /31024/11, /31024/12,
     """
-    client_id,  = re.findall(r"Client #([0-9]+) updated.", server_output)
+    print("========parsed top   ==========")
+    print(server_output)
+    print("========parsed bottom==========")
+    client_id, = re.findall(r".lient #([0-9]+) ", server_output)
+    action, = re.findall(r"(registered|updated)", server_output)
     endpoint, = re.findall(r"name: \"([\w-]+)\"\r\r\n", server_output)
     version, = re.findall(r"version: \"([0-9.]+)\"\r\r\n", server_output)
     binding, = re.findall(r"binding: \"([A-Z]+)\"\r\r\n", server_output)
     lifetime, = re.findall(r"lifetime: ([0-9]+) sec\r\r\n", server_output)
     objects = re.search(r"objects: ([\/0-9]+ ?(\([0-9.]+\))?, ?)+\r\r\n", server_output)
-    return int(client_id), endpoint, version, binding, int(lifetime), objects.string
+    return int(client_id), action, endpoint, version, binding, int(lifetime), objects.string
 
 
-def tXXest_registration_interface(lwm2mserver, lwm2mclient):
+def test_registration_interface(lwm2mserver, lwm2mclient):
     """LightweightM2M-1.1-int-101
     Test that the Client registers with the Server."""
     lwm2mserver.expect_exact("\r\r\n>")
-    client_id, endpoint, version, binding, lifetime, objects = \
+    client_id, action, endpoint, version, binding, lifetime, objects = \
         parse_client_registration(lwm2mserver.before)
     assert client_id == 0
+    assert action == "registered"
     assert endpoint == "testlwm2mclient"
     assert version == "1.1"
     assert binding == "UDP"
@@ -48,6 +53,7 @@ def test_registration_update_trigger(lwm2mserver, lwm2mclient):
     with the Registration Update Trigger."""
     lwm2mserver.expect_exact("\r\r\n>")
     print(lwm2mserver.before)
+    print("1----------------------------------------------------------------------------")
     '''
     client_id, endpoint, version, binding, lifetime, objects = \
         parse_client_registration(lwm2mserver.before)
@@ -62,16 +68,19 @@ def test_registration_update_trigger(lwm2mserver, lwm2mclient):
     lwm2mserver.sendline("exec 0 /1/0/8")
     lwm2mserver.expect_exact("\r\r\n>")
     print(lwm2mserver.before)
+    print("2----------------------------------------------------------------------------")
     lwm2mserver.expect_exact("\r\r\n>")
     print(lwm2mserver.before)
+    print("3----------------------------------------------------------------------------")
     lwm2mserver.expect_exact("\r\r\n>")
     #print(lwm2mserver.before)
 
     #lwm2mserver.expect_exact("updated")
     #print(lwm2mserver.before)
-    client_id, endpoint, version, binding, lifetime, objects = \
+    client_id, action, endpoint, version, binding, lifetime, objects = \
         parse_client_registration(lwm2mserver.before)
     assert client_id == 0
+    assert action == "updated"
     assert endpoint == "testlwm2mclient"
     assert version == "1.1"
     assert binding == "UDP"
