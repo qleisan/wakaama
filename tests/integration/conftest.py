cc66510058d7b7b5d0b0ef5d3601f52b7cc2529e
Leif Sandstrom <leif.sandstrom@cybercom.com>
2021-05-25 12:25:20 +0200

    Add more integration tests
    Add int-102, int-105, int-301, int-302

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,21 @@
 """Wakaama integration tests (pytest) fixtures"""
 from pathlib import Path
+from time import sleep
 import pytest
 import pexpect
+
+
+def get_senml_json_record(parsed, urn, label):
+    """helper function returning value of associated label
+
+    example SenML input:
+
+        [{'bn': '/1/', 'n': '0/0', 'v': 123}, {'n': '0/1', 'v': 300},
+        {'n': '0/2', 'v': 0}, {'n': '0/3', 'v': 0}, {'n': '0/5', 'v': 0},
+        {'n': '0/6', 'vb': False}, {'n': '0/7', 'vs': 'U'}]"""
+
+    return next(item for item in parsed if item["n"] == urn)[label]
+
 
 # pylint: disable=no-member
 class HelperBase:
@@ -45,6 +59,13 @@ class HelperBase:
             self.__dumptext()
             return False
         return True
+
+    def waitfortime(self, timedelay):
+        """Wait for time and return output since last command"""
+        sleep(timedelay)
+        # this is a "hack" to be able to return output between commands
+        self.commandresponse("help", "help")
+        return self.pexpectobj.before
 
     def quit(self):
         """Quit client or server"""
